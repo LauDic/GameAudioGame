8d3ede798fa69fe391f4598daf5cc32e520a9300
LauDic <lautarodichio@hotmail.com>
2023-07-31 19:01:04 -0300

    intermediate

---
+++ Intermediate/Source/FirstGame.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class FirstGame : ModuleRules
+{
+	public FirstGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
